f26b6bcabfbd6bf5b42f5c3a3e11c7b39a4ecbea
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-02-14 19:00:19 -0500

    setting up new project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(document.getElementById('root'));
+
+
+//git add . && git commit -m "setting up new project" && git push && git status
