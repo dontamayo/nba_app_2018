2bc81712e306c79398152968fbe75a14cd9d8244
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-02-14 19:36:43 -0500

    added router.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(document.getElementById('root'));
+import Routes from './routes';
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
+  )
+}
+
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 //git add . && git commit -m "setting up new project" && git push && git status
