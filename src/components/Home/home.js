a4d42b99e2be4b74db9395d64c8384e6a12d15ef
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-02-19 20:31:00 -0500

    modified readme, json, home, added widgets, installed axios

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import NewsSlider from '../widgets/NewsSlider/slider';
+
+
 const Home = () => {
   return(
     <div>
-      Home 
+      <NewsSlider/>
     </div>
   )
 }
---
+++ src/components/widgets/NewsSlider/slider.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class NewsSlider extends Component {
+
+  state = {
+    news: []
+  }
+
+  render(){
+    return(
+      <div>
+        SLIDER mania
+      </div>
+    )
+  }
+}
+
+export default NewsSlider;
