220926084bc97355972aa167a0b3594cca478188
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-02-22 15:32:52 -0500

    modified Videoslist template, routes, addedfolders articles(elements,news,videos)

--- src/routes.js
+++ src/routes.js
@@ -4,17 +4,26 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
 
-class Routes extends Component {
-  render(){
-    return(
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Home}/>
-        </Switch>
-      </Layout>
+import NewsArticle from './components/Articles/News/Post/index';
+import VideoArticle from './components/Articles/Videos/Video/index'
+import NewsMain from './components/Articles/News/Main/index';
+import VideosMain from './components/Articles/Videos/Main/index';
 
-    )
-  }
+class Routes extends Component {
+    render(){
+        return(
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/news" exact component={NewsMain}/>
+                    <Route path="/articles/:id" exact component={NewsArticle}/>
+                    <Route path="/videos/:id" exact component={VideoArticle}/>
+                    <Route path="/videos" exact component={VideosMain}/>
+                </Switch>
+            </Layout>
+
+        )
+    }
 }
 
 export default Routes;
